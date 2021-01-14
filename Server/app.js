1c5e568cd12dce57d856aeb0796766aebe2412f9
Hosam <habdeltawab@qci.com>
2021-01-14 09:26:52 -0600

    added mongoose to connect to mLab

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//Connect to mlab DB
+mongoose.connect('mongodb+srv://hosam:test123@graphql-hosam.nrk8q.mongodb.net/graphql-hosam?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('Connected to DB...');
+})
 
 app.use(
   "/graphql",
