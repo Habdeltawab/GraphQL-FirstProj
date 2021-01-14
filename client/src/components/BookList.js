e2a6cc586e9468ca18f8f52a3ab35c2f0ce8839d
Hosam <habdeltawab@qci.com>
2021-01-14 13:46:24 -0600

    set-up Apollo client

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul id="book-list">
+          <li>Book Name</li>
+        </ul>
+      </div>
+    );
+  }
+}
