7806e2ada9889f3bfaa470ad05b47b12866c95d5
Hosam <habdeltawab@qci.com>
2021-01-15 07:34:38 -0600

    added a form for adding a new book

--- Server/app.js
+++ Server/app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+// Allow cross origin requests
+app.use(cors());
+
 //Connect to mlab DB
-mongoose.connect('mongodb+srv://hosam:test123@graphql-hosam.nrk8q.mongodb.net/graphql-hosam?retryWrites=true&w=majority');
-mongoose.connection.once('open', () => {
-    console.log('Connected to DB...');
-})
+mongoose.connect(
+  "mongodb+srv://hosam:test123@graphql-hosam.nrk8q.mongodb.net/graphql-hosam?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB...");
+});
 
 app.use(
   "/graphql",
